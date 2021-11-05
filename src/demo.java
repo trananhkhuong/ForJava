dba0424ccdf48eaef640efc365508198656009d9
trananhkhuong <khuongt711@gmail.com>
2021-11-05 16:13:57 +0700

    hi

---
+++ src/demo.java
@@ -0,0 +1,14 @@
+public class demo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5 ; i--) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+                for (int k = 0; k < 5; k++) {
+                    System.out.print(" *");
+                }
+
+            }
+            System.out.print("*");
+        }
+    }
+}
--- src/prime20.java
+++ src/prime20.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class prime20 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
 
-        for (int i = 2; i < 20; i++) {
+
+        for (int i = 2; i < number; i++) {
             boolean check = true;
             for (int j = 2; j < Math.sqrt(i); j++) {
                 if (i % j == 0){
